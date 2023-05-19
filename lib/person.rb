1fe4656701cd5746f0bfe6d64e4ec7fe3129beef
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-19 16:55:56 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,43 @@
 class Person
+    def initialize(name, job, age)
+      @name = name
+      @job = job
+      @age = age
+    end
+  
+    def name
+      @name
+    end
+  
+    def job
+      @job
+    end
+  
+    def age
+      @age
+    end
+  
+    # Setter methods
+    def name=(new_name)
+      @name = new_name
+    end
+  
+    def job=(new_job)
+      @job = new_job
+    end
+  
+    def age=(new_age)
+      @age = new_age
+    end
+  end
+  
+  person4 = Person.new("Benayah", "Developer", 45)
+  puts "#{person4.name} is a good #{person4.job} and his age is #{person4.age}"
+  
+  person4.name = "Sherlock"
+  person4.job = "Detective"
+  person4.age = 27
 
-end
+  puts "#{person4.name} is a good #{person4.job} and his age is #{person4.age}"
+
+  
